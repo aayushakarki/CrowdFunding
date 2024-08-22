35401d0dbde918ebb54d09d997ea60ad8a273fe8
aayusha <aayushakarki24@gmail.com>
2024-08-22 07:34:12 +0545

    finally completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,30 +5,102 @@ import { DisplayCampaigns } from "../components";
 const Home = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [campaigns, setCampaigns] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState('All Campaigns');
   const { contract, getCampaigns, searchQuery } = useStateContext();
-  
+
   const fetchCampaigns = async () => {
     setIsLoading(true);
     const data = await getCampaigns();
     setCampaigns(data);
     setIsLoading(false);
   }
-  
+
   useEffect(() => {
     if (contract) fetchCampaigns();
   }, [contract]);
 
-  const filteredCampaigns = campaigns.filter(campaign =>
-    campaign.title.toLowerCase().startsWith(searchQuery.toLowerCase())
-  );
+  const handleCategoryClick = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const filteredCampaigns = campaigns.filter(campaign => {
+    const matchesCategory = selectedCategory === 'All Campaigns' || campaign.category.toLowerCase() === selectedCategory.toLowerCase();
+    const matchesSearchQuery = campaign.title.toLowerCase().startsWith(searchQuery.toLowerCase());
+    return matchesCategory && matchesSearchQuery;
+  });
 
   return (
-    <DisplayCampaigns 
-      title="All Campaigns"
-      isLoading={isLoading}
-      campaigns={filteredCampaigns}
-    />
-  )
+    <div>
+      <div className="flex space-x-4 mb-4 overflow-auto">
+        <button
+        className={`py-2 px-4 rounded-[10px] font-epilogue font-medium text-[14px] transition-transform duration-300 ${
+          selectedCategory === 'All Campaigns'
+          ? 'bg-[#15192D] text-green-500'
+          : 'bg-[#000000] text-[#b2b3bd] hover:bg-[#4b4b61] hover:text-green-500 scale-105'
+        }`}
+        onClick={() => handleCategoryClick('All Campaigns')}
+        >
+        All Campaigns
+        </button>
+        <button
+          className={`py-2 px-4 rounded-[10px] font-epilogue font-medium text-[14px] transition-transform duration-300 ${
+            selectedCategory === 'Health' 
+            ? 'bg-[#15192D] text-green-500' 
+            : 'bg-[#000000] text-[#b2b3bd] hover:bg-[#15192D] hover:text-green-500 scale-105'
+          }`}
+          onClick={() => handleCategoryClick('Health')}
+        >
+        Health
+        </button>
+        <button
+          className={`py-2 px-4 rounded-[10px] font-epilogue font-medium text-[14px] transition-transform duration-300 ${
+            selectedCategory === 'Education' 
+            ? 'bg-[#15192D] text-green-500' 
+            : 'bg-[#000000] text-[#b2b3bd] hover:bg-[#15192D] hover:text-green-500 scale-105'
+          }`}
+          onClick={() => handleCategoryClick('Education')}
+        >
+          Education
+        </button>
+        <button
+          className={`py-2 px-4 rounded-[10px] font-epilogue font-medium text-[14px] transition-transform duration-300 ${
+            selectedCategory === 'Humanitarian' 
+            ? 'bg-[#15192D] text-green-500' 
+            : 'bg-[#000000] text-[#b2b3bd] hover:bg-[#15192D] hover:text-green-500 scale-105'
+          }`}
+          onClick={() => handleCategoryClick('Humanitarian')}
+        >
+          Humanitarian
+        </button>
+        <button
+          className={`py-2 px-4 rounded-[10px] font-epilogue font-medium text-[14px] transition-transform duration-300 ${
+            selectedCategory === 'Technology and Innovation' 
+            ? 'bg-[#15192D] text-green-500' 
+            : 'bg-[#000000] text-[#b2b3bd] hover:bg-[#15192D] hover:text-green-500 scale-105'
+          }`}
+          onClick={() => handleCategoryClick('Technology and Innovation')}
+        >
+          Technology and Innovation
+        </button>
+        <button
+          className={`py-2 px-4 rounded-[10px] font-epilogue font-medium text-[14px] transition-transform duration-300 ${
+            selectedCategory === 'Emergency and Disaster Relief' 
+            ? 'bg-[#15192D] text-green-500' 
+            : 'bg-[#000000] text-[#b2b3bd] hover:bg-[#15192D] hover:text-green-500 scale-105'
+          }`}
+          onClick={() => handleCategoryClick('Emergency and Disaster Relief')}
+        >
+          Emergency and Disaster Relief
+        </button>
+      </div>
+
+      <DisplayCampaigns 
+        title={selectedCategory}
+        isLoading={isLoading}
+        campaigns={filteredCampaigns}
+      />
+    </div>
+  );
 }
 
 export default Home;
--- web3/contracts/ProjectCrowdfunding.sol
+++ web3/contracts/ProjectCrowdfunding.sol
@@ -15,10 +15,16 @@ contract CrowdFunding {
         uint256[] donations;
     }
 
+    // In Solidity, "mapping" is a data structure used to associate a key with a value. 
+    // It's similar to a hash table or dictionary in other programming languages.
     mapping(uint256 => Campaign) public campaigns;
 
-    uint256 public numberOfCampaigns = 0;
+    uint256 public numberOfCampaigns = 0; // "public" keyword is visibility modifier, it specifies visibility of function
 
+    // a function declared "public" can be called from outside the contract
+    // and can be accessed by any other contract or externally owned account (EOA) 
+    // on the Ethereum blockchain.
+    // this func. returns ID of the created campaign
     function createCampaign(address _owner, string memory _title, string memory _description, 
         uint256 _target, uint256 _deadline, string memory _category, string memory _image) public returns (uint256) {
         
@@ -39,6 +45,10 @@ contract CrowdFunding {
         return (numberOfCampaigns - 1);
     }
 
+    // The payable keyword indicates that this function can receive Ether. 
+    // Users can send Ether to this function when they call it.
+    // The amount of Ether (in Wei) sent with the transaction is accessible    
+    // within the function via the msg.value variable.
     function donateToCampaign(uint256 _id) public payable { 
         require(_id < numberOfCampaigns, "Campaign ID does not exist.");
 
@@ -57,12 +67,20 @@ contract CrowdFunding {
     function getDonators(uint256 _id) public view returns (address[] memory, uint256[] memory) {
         require(_id < numberOfCampaigns, "Campaign ID does not exist.");
 
+        // In Solidity, when you declare a variable with the storage keyword, 
+        // you're creating a reference to a storage slot rather than creating a 
+        // new copy of the data. So, when you modify a variable declared with storage, 
+        // you're directly modifying the data in storage, which reflects on the global state.
+
+        // the local variable campaign is a pointer to a slot on global campaigns
         Campaign storage campaign = campaigns[_id];
         return (campaign.donators, campaign.donations);
     }
 
     function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+        // allCampaigns is array of elements of type Campaign[] structures
+        // the size of array is equal to numberOfCampaigns
         for (uint256 i = 0; i < numberOfCampaigns; i++) {
             allCampaigns[i] = campaigns[i];
         }
